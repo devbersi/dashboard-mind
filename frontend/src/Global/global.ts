0e9d954859de338463e9f67bd612021b7b6f01e9
devbersi <guibersi.13@outlook.com>
2021-12-19 23:49:48 -0300

    Refatorando styled para sass básico, montando dashboard

--- frontend/src/Global/global.ts
+++ frontend/src/Global/global.ts
@@ -43,6 +43,7 @@ export const GlobalStyle = createGlobalStyle`
 
     h1, h2, h3, h4, h5, h6, strong {
         font-weight: 600;
+        color: white;
     }
 
     button {
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,23 +1,51 @@
 import React, { useState } from "react";
-import { DashboardPage, SideBar, DivClose, DivLogOut, DivButtons, SideBarInative } from "./styles/dashboard";
 import { IoMdClose } from 'react-icons/io';
+import { Link } from "react-router-dom";
+import Logo from '../../images/logo.png';
+import LogoutIcon from '@mui/icons-material/Logout';
+import './styles/dashboard.scss';
+import MaterialTable from 'material-table'
 
 export const Dashboard = () => {
     const [navbar, setNavbar ] = useState(true);
 
   return (
-      <DashboardPage>
-          <SideBar>
-              <DivClose><IoMdClose/></DivClose>
-              <DivButtons>
-                  <p>Home</p>
-                  <p>Dashboard</p>
-              </DivButtons>
-              <DivLogOut>
-                  <p>Sair</p>
-              </DivLogOut>
-          </SideBar>
-          <div></div>
-      </DashboardPage>
+      <div className="DashboardPage">
+          <div className="Navbar">
+            <img className="ImageLogo" src={Logo} alt="Mind Consulting" />
+            <button className="ExitButton"><LogoutIcon/>Exit</button>
+          </div>
+          <div className="ContentDiv">
+            <h3>Dashboard</h3>
+            <MaterialTable
+      title="Free Action Preview"
+      columns={[
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ]}
+      data={[
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+      ]}        
+      actions={[
+        {
+          icon: 'add',
+          tooltip: 'Add User',
+          isFreeAction: true,
+          onClick: (event) => alert("You want to add a new row")
+        }
+      ]}
+    />
+          </div>
+          <div className="SecondContentDiv">
+            <h3>table</h3>
+          </div>
+      </div>
   );
 };
